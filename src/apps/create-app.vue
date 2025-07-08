d99a324795a1e1f86ca09617879d53c487cb1143
Even Galland Alander <even@alander.org>
2025-07-08 12:48:01 +0200

    cleanup index.html, barely started on create-app

--- src/apps/create-app.vue
+++ src/apps/create-app.vue
@@ -1,11 +1,19 @@
 <template>
   <form>
-    <textarea v-model="description"></textarea>
+    <textarea
+      v-model="description"
+      placeholder="f.eks. Nydelige hveteboller med surdeig"
+    >
+    </textarea>
   </form>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 
+import { Ingredient, Step } from '@/types';
+
 const description = ref<string>('');
+const ingredients: Ingredient[] = [];
+const steps: Step[] = [];
 </script>
